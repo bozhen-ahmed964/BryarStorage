6b5361df26ac1fd5f7a73b2dc22302cb3eef0823
Bryareqbal <briar.eqbal@gmail.com>
2023-12-21 11:20:07 +0300

    slaw

--- resources/views/layouts/include/details.blade.php
+++ resources/views/layouts/include/details.blade.php
@@ -1,6 +1,12 @@
-<!-- Bottom Navigation -->
-<nav class="navbar navbar-expand-lg fixed-bottom" style="background-color: #1E1E1F">
+<div class="navbar navbar-expand-lg fixed-bottom" style="background-color: #1E1E1F">
     <div class="container">
-        <span class="text-white">2 Selected of 112 itmes <strong>300.66 MB</strong></span>
+        <div class="row w-100">
+            <div class="col-12 col-md-6 text-center text-md-start">
+                <span class="text-white">2 Selected of 112 items <strong>300.66 MB</strong></span>
+            </div>
+            <div class="col-12 col-md-6 text-center text-md-end">
+                <span class="text-white">2 Selected of 112 items <strong>300.66 MB</strong></span>
+            </div>
+        </div>
     </div>
-</nav>
+</div>
