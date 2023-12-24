461fe8337e50497f32e63606b2fd433dfbdf4647
bozhen964 <bozhencomputer964@gmail.com>
2023-12-24 20:47:09 +0300

    fixing main section

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -54,18 +54,17 @@
 
 
 
+
+    @include('layouts.include.leftSidebar')
+    @include('layouts.include.navbar')
     <div>
-        @include('layouts.include.leftSidebar')
-        @include('layouts.include.navbar')
-        <div class="main mt-5 p-4 bg-danger rounded">
-            @include('user.dashboard')
-        </div>
-        <div class="right-sidebar" style="margin-top: 80px">
-            @include('layouts.include.rightSidebar')
-        </div>
+        @yield('content')
     </div>
 
-    
+    @include('layouts.include.rightSidebar')
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -3,141 +3,245 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1 class="mt-5">All Folder Here</h1>
-    <div class="row mt-5 ">
-        <div class="col-md-3" style="margin-top: 10px">
-            <div class="card border rounded border-1 shadow">
-                <div class="card-body border rounded">
-                    <div class="row">
-                        <div class="col">
-                            <span style="font-weight: bold; font-size: 12px">8 files</span>
+    <div class="main mt-5 p-4 bg-danger rounded">
+        <h1 class="text-light">All Folder Here</h1>
+        <div class="row mt-5 ">
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
+                                        class="far fa-user" style="font-weight: bold"></i> 4</span>
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right">
-                            <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
-                                    class="far fa-user" style="font-weight: bold"></i> 4</span>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
                         </div>
-                    </div>
-                    <div style="text-align: center">
-                        <i class="fas fa-folder" style="font-size: 100px"></i>
-                    </div>
-                    <div class="row">
-                        <div class="col" style="font-size: 12px">
-                            <p>All Content</p>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col">
-                            <div style="text-align: right">
-                                <i class="far fa-star" style="font-size: 18px"></i>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3" style="margin-top: 10px">
-            <div class="card border rounded border-1 shadow">
-                <div class="card-body border rounded">
-                    <div class="row">
-                        <div class="col">
-                            <span style="font-weight: bold; font-size: 12px">8 files</span>
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
+                                        class="far fa-user" style="font-weight: bold"></i> 4</span>
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right">
-                            <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
-                                    class="far fa-user" style="font-weight: bold"></i> 4</span>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
                         </div>
-                    </div>
-                    <div style="text-align: center">
-                        <i class="fas fa-folder" style="font-size: 100px"></i>
-                    </div>
-                    <div class="row">
-                        <div class="col" style="font-size: 12px">
-                            <p>All Content</p>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col">
-                            <div style="text-align: right">
-                                <i class="far fa-star" style="font-size: 18px"></i>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3" style="margin-top: 10px">
-            <div class="card border rounded border-1 shadow">
-                <div class="card-body border rounded">
-                    <div class="row">
-                        <div class="col">
-                            <span style="font-weight: bold; font-size: 12px">8 files</span>
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
+                                        class="far fa-user" style="font-weight: bold"></i> 4</span>
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right">
-                            <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
-                                    class="far fa-user" style="font-weight: bold"></i> 4</span>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
                         </div>
-                    </div>
-                    <div style="text-align: center">
-                        <i class="fas fa-folder" style="font-size: 100px"></i>
-                    </div>
-                    <div class="row">
-                        <div class="col" style="font-size: 12px">
-                            <p>All Content</p>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col">
-                            <div style="text-align: right">
-                                <i class="far fa-star" style="font-size: 18px"></i>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3" style="margin-top: 10px">
-            <div class="card border rounded border-1 shadow">
-                <div class="card-body border rounded">
-                    <div class="row">
-                        <div class="col">
-                            <span style="font-weight: bold; font-size: 12px">8 files</span>
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end"
+                                    style="font-weight: bold; font-size: 12px"><i class="far fa-user"
+                                        style="font-weight: bold"></i> 4</span>
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right">
-                            <span class="d-xl-flex justify-content-xl-end" style="font-weight: bold; font-size: 12px"><i
-                                    class="far fa-user" style="font-weight: bold"></i> 4</span>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
                         </div>
-                    </div>
-                    <div style="text-align: center">
-                        <i class="fas fa-folder" style="font-size: 100px"></i>
-                    </div>
-                    <div class="row">
-                        <div class="col" style="font-size: 12px">
-                            <p>All Content</p>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col">
-                            <div style="text-align: right">
-                                <i class="far fa-star" style="font-size: 18px"></i>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end"
+                                    style="font-weight: bold; font-size: 12px"><i class="far fa-user"
+                                        style="font-weight: bold"></i> 4</span>
+                            </div>
+                        </div>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
+                        </div>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end"
+                                    style="font-weight: bold; font-size: 12px"><i class="far fa-user"
+                                        style="font-weight: bold"></i> 4</span>
+                            </div>
+                        </div>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
+                        </div>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3" style="margin-top: 10px">
+                <div class="card border rounded border-1 shadow">
+                    <div class="card-body border rounded">
+                        <div class="row">
+                            <div class="col">
+                                <span style="font-weight: bold; font-size: 12px">8 files</span>
+                            </div>
+                            <div class="col" style="text-align: right">
+                                <span class="d-xl-flex justify-content-xl-end"
+                                    style="font-weight: bold; font-size: 12px"><i class="far fa-user"
+                                        style="font-weight: bold"></i> 4</span>
+                            </div>
+                        </div>
+                        <div style="text-align: center">
+                            <i class="fas fa-folder" style="font-size: 100px"></i>
+                        </div>
+                        <div class="row">
+                            <div class="col" style="font-size: 12px">
+                                <p>All Content</p>
+                            </div>
+                            <div class="col">
+                                <div style="text-align: right">
+                                    <i class="far fa-star" style="font-size: 18px"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                                style="width: 50%;">50%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-    </div>
-    <div class="text-center mt-5 bg-light">
-        <div class="table-responsive rounded">
-            <table class="table table-sm table-borderless">
-                <tbody class="rounded-5">
-                    <tr>
-                        <td>
-                            <svg class="bi bi-image fs-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-                                fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
-                                <path
-                                    d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z">
-                                </path>
-                            </svg>
-                        </td>
-                        <td>Image05.jpg</td>
-                        <td>Jun 10th 2021</td>
-                        <td>1.5MB</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-@endsection
+
+    @endsection
