b8c98481acd53f1fbdc59db9b0a405e28707434d
Bryareqbal <briar.eqbal@gmail.com>
2024-01-15 11:44:32 +0300

    without navbar

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
     <div class="col-md-10 p-4 rounded">
-        <div class="container mt-3">
-            <div class="w-100 h-10 p-2 rounded">
+        <div class="container-fluid mt-3">
+            <div class="row w-100 h-10 p-2 rounded">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="input-group">
@@ -24,12 +24,12 @@
                 </div>
 
             </div>
-            <div class="row mt-4">
-                <div class="col-md-6 text-start border rounded p-1">
+            <div class="row mx-2 mt-4">
+                <div class="col-md-6 border rounded p-1">
 
-                    <h6 class="text-dark fs-4 text-capitalize text-opacity-50 "> <a href="#">
+                    <h6 class="text-dark fs-5 mt-1 text-capitalize text-opacity-50"> <a href="#">
 
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 384 512">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="24" viewBox="0 0 384 512">
                                 <path
                                     d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                             </svg></a> ( 5 ) folder selected | total size 3GB
@@ -37,7 +37,7 @@
                 </div>
             </div>
 
-            <div class="w-100 h-10 p-2 rounded">
+            <div class="row w-100 h-10 p-2 rounded">
                 <div class="row">
 
                     <div class="col-md-6 text-start">
